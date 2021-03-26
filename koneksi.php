f5619559fb645710b671f74af45c39e2ae6b9cca
Erdin Maulana <68404251+Erdinmaulana21@users.noreply.github.com>
2021-03-26 09:05:12 +0700

    Add files via upload

---
+++ koneksi.php
@@ -0,0 +1,7 @@
+<?php
+$host       ="localhost";
+$user       ="root";
+$password   ="";
+$database   ="dinamis";
+$connect    =mysqli_connect($host,$user,$password,$database) or die("Gagal Menghubungkan");
+?>
